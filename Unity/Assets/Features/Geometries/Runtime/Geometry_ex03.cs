bf4e5d0873aa36e03f6f1572b9c9fa27f2e40d07
niborobin19 <56249261+niborobin19@users.noreply.github.com>
2021-05-25 17:23:24 +0200

    Geometry Exercice 03

---
+++ Unity/Assets/Features/Geometries/Runtime/Geometry_ex03.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+namespace Geometries.Runtime
+{
+    public class Geometry_ex03
+    {
+        #region Public
+
+        #endregion
+
+
+        #region Constructors
+        public Geometry_ex03(float handWidth)
+        {
+            
+        }
+        #endregion
+
+
+        #region Main
+
+        /// 
+        #endregion
+    }
+}
---
+++ Unity/Assets/Features/Geometries/Tests/Geometry_ex03_Test.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Dynamic;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Geometries.Runtime;
+
+public class Geometry_ex03_Test
+{
+    #region Tests
+    [SetUp]
+    public void SetUpTests()
+    {
+        
+    }
+
+    [Test]
+    public void Geometry_ex03_Test1()
+    {
+
+    }
+
+    [TearDown]
+    public void TearDownTests()
+    {
+        
+    }
+
+    #endregion
+
+    #region Private
+    
+    #endregion
+}
