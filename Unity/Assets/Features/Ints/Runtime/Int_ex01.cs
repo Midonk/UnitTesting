1bf1f45a8aa4c78a7d7e9ff5b6542a6fdef25637
KyanosAlpha <66301729+KyanosAlpha@users.noreply.github.com>
2021-05-25 16:43:02 +0200

    Commit Int_Ex01

--- Unity/Assets/Features/Ints/Runtime/Int_ex01.cs
+++ Unity/Assets/Features/Ints/Runtime/Int_ex01.cs
@@ -2,9 +2,56 @@ using UnityEngine;
 
 namespace Ints
 {
-    /// ENNONCE HERE
-    public class Int_ex01
+    public class Int_Ex01
     {
+        /// Aujourd'hui, tu as décidé de prendre ta santé en main et d'acheter des pommes, tu vas au supermaché le plus proche en voiture, le temps d'arriver
+        /// au magasin, tu as su fumé ta cigarette, tu arrives à l'entrée du centre commercial, passant par l'escalateur et ayant fini ta canette de bière, 
+        /// tu arrives enfin au Cora, tu te dirige vers le rayon où ce trouve les pommes.
+        /// Une fois les pommes trouver, tu ne sais pas lesquels prendre, tu te tates les boules pour comparer la fermeté du fruit,
+        /// tu es satisfait des pommes et en prends quelques unes.
+        /// Tu te diriges enfin à la caisse et la caissière qui ne remarque pas ton charme implaccable, ne rétorque pas à ta lourdeur à propos de son décolté, 
+        /// fait passé tes articles assez rapidement.
+        /// La caissière voulant aller trop vite pour que tu partes vite, fit buggé sa machine enregistreuse et te demande poliment de calculer la somme du
+        /// prix de toutes tes pommes: "Les pommes que vous voulez coutent 3euros, Monsieurs", si vous réussisez, la caissière vous a promis de te donner 
+        /// son numéro de téléphone.
+        public int CalculateApplesPrices(int pommes)
+        {
+            return 0;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public Int_Ex01()
+        {
+
+        }
 
     }
 }
--- Unity/Assets/Features/Ints/Tests/INTS_SHOULD.cs
+++ Unity/Assets/Features/Ints/Tests/INTS_SHOULD.cs
@@ -5,7 +5,20 @@ using UnityEngine;
 using UnityEngine.TestTools;
 using Ints;
 
+using TestIntEx01 = Ints.Int_Ex01;
+
 public class INTS_SHOULD
 {
+    [Test]
+    public void TEST_EX01()
+    {
+        //given
+        var testInt01 = new TestIntEx01();
 
+        //when
+        var result = testInt01.CalculateApplesPrices(8);
+
+        //then
+        Assert.IsTrue(result == (8 * 3));
+    }
 }
