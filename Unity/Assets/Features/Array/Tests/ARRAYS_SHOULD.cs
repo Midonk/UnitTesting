fed90a663d2b0fd6dea174b2316c4dd747737934
Breniak <kyllian_bocklandt@hotmail.com>
2021-05-25 20:51:51 +0200

    added Array_ex02.cs

--- Unity/Assets/Features/Array/Tests/ARRAYS_SHOULD.cs
+++ Unity/Assets/Features/Array/Tests/ARRAYS_SHOULD.cs
@@ -7,19 +7,6 @@ using Arrays;
 
 public class ARRAYS_SHOULD
 {
-    //[Test]
-    //public void TEST_TEMPLATE()
-    //{
-    //    // Given
-
-
-    //    // When
-
-
-    //    // Then
-
-    //}
-
     [Test]
     public void RETURN_15_FROM_INT_ARRAY()
     {
@@ -31,11 +18,11 @@ public class ARRAYS_SHOULD
         var result = ArrayEx01.GetHighestStrategicalValuePoint(testArray);
 
         // Then
-        Assert.IsTrue(Mathf.Approximately(result, 15));
+        Assert.IsTrue(result == 15);
     }
 
     [Test]
-    public void RETURN_NEGATIVE_5_FROM_INT_ARRAY()
+    public void RETURN_NEGATIVE_5_FROM_NEGATIVE_INT_ARRAY()
     {
         // Given
         var testArray = new int[] { -5, -50, -10, -20, -12, -25 };
@@ -45,6 +32,36 @@ public class ARRAYS_SHOULD
         var result = ArrayEx01.GetHighestStrategicalValuePoint(testArray);
 
         // Then
-        Assert.IsTrue(Mathf.Approximately(result, -5));
+        Assert.IsTrue(result == -5);
+    }
+
+    [Test]
+    public void REPLACE_NULL_IN_ARRAY()
+    {
+        // Given
+        var testArray = new string[] { null, "gear", "stuff", null, "cable", null };
+        var ArrayEx02 = new Array_ex02();
+        string part = "new part";
+
+        // When 
+        ArrayEx02.ReplaceDamagedParts(testArray, part);
+
+        // Then
+        Assert.IsTrue(testArray[0] == part && testArray[3] == part && testArray[5] == part);
+    }
+        
+    [Test]
+    public void NOT_REPLACE_NOT_NULL_ITEM_IN_ARRAY()
+    {
+        // Given        
+        var testArray = new string[] { null, "gear", "stuff", null, "cable", null };
+        var ArrayEx02 = new Array_ex02();
+        string part = "new part";
+
+        // When 
+        ArrayEx02.ReplaceDamagedParts(testArray, part);
+
+        // Then
+        Assert.IsTrue(testArray[1] == "gear" && testArray[2] == "stuff" && testArray[4] == "cable");
     }
 }
