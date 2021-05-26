a32357a27e4e7ab22ce3aa38cd8501535e6f95e6
Amaury Deltenre <amaury.deltenre@gmail.com>
2021-05-26 15:26:30 +0200

    init exo 2

---
+++ Unity/Assets/Features/Floats/Runtime/Float_ex02.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Float
+{
+    public class Float_ex02
+    {
+        /// 
+        /// 
+        /// 
+
+
+        // #region Public
+        // public float ;
+        // #endregion
+
+
+        // #region Constructor
+        // public Float_ex02(float )
+        // {
+
+        // }
+        // #endregion
+
+
+        // #region Main
+        // public float (float value)
+        // {
+
+        // }
+        // #endregion
+    }
+}
---
+++ Unity/Assets/Features/Floats/Tests/Test02.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using TestFloat02 = Float.Float_ex02;
+
+public class Test02
+{
+    [Test]
+    public void TEST_EX02()
+    {
+        // Given
+
+
+        // When
+
+
+        // Then
+
+    }
+}
+
