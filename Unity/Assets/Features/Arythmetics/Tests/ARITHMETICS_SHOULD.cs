9b7823c75061e1867842ba026093039f23b50c47
EladarArunsun <herve.chiera@gmail.com>
2021-05-26 13:40:50 +0200

    Exercise 02

--- Unity/Assets/Features/Arythmetics/Tests/ARITHMETICS_SHOULD.cs
+++ Unity/Assets/Features/Arythmetics/Tests/ARITHMETICS_SHOULD.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 using UnityEngine.TestTools;
 
 using Ex01 = Arithmetics.Arithmetic_ex01;
+using Ex02 = Arithmetics.Arithmetic_ex02;
+using Ex03 = Arithmetics.Arithmetic_ex03;
 
 public class ARITHMETICS_SHOULD
 {
     [Test]
-    public void ARITHMETICS()
+    public void SOLVE01()
     {
         // Given
         var myArithmetics = new Ex01(24f);
@@ -20,4 +22,19 @@ public class ARITHMETICS_SHOULD
         // Then
         Assert.IsTrue(result == Mathf.Sqrt(24.0f)-4f);
     }
+
+    [Test]
+    public void SOLVE02()
+    {
+        // Given
+        var myArithmetics = new Ex02((1.2f * 1.2f) + (1.5f * 1.5f));
+
+        // When
+        var result = myArithmetics.CalculateHypothesis(Mathf.Sqrt(1.2f * 1.2f + 1.5f * 1.5f));
+
+        // Then
+        Assert.IsTrue(result == (Mathf.Sqrt(1.2f * 1.2f + 1.5f * 1.5f)));
+    }
+
+    
 }
