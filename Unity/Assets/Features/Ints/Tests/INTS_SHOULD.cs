b1a5f44bb2cde9797c908845cee0e16baba7cc51
KyanosAlpha <66301729+KyanosAlpha@users.noreply.github.com>
2021-05-26 09:28:43 +0200

    Commit Int_Ex02

--- Unity/Assets/Features/Ints/Tests/INTS_SHOULD.cs
+++ Unity/Assets/Features/Ints/Tests/INTS_SHOULD.cs
@@ -5,20 +5,33 @@ using UnityEngine;
 using UnityEngine.TestTools;
 using Ints;
 
-using TestIntEx01 = Ints.Int_Ex01;
-
 public class INTS_SHOULD
 {
     [Test]
     public void TEST_EX01()
     {
         //given
-        var testInt01 = new TestIntEx01();
+        var testInt01 = new Ints.Int_Ex01();
 
         //when
         var result = testInt01.CalculateApplesPrices(8);
 
         //then
         Assert.IsTrue(result == (8 * 3));
+    }
+
+    [Test]
+    public void TEST_EX02()
+    {
+        //given
+        var testInt02 = new Ints.Int_Ex02();
+        var phoneNumber = "000446157643";
+        var intPhone = int.Parse(phoneNumber);
+
+        //when
+        var result = testInt02.PhoneNumber(phoneNumber);
+
+        //then
+        Assert.IsTrue(intPhone == result);
     }
 }
