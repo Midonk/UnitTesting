8f04bb85cc5ce131daa1a7f51d57d15e8a519260
kfaoofyrbzcbfdadfj <kfaoofyrbzcbfdadfj@twzhhq.online>
2021-05-26 15:48:32 +0200

    avant le merge de develop pour mettre à jour

--- Unity/Assets/Features/Quaternions/Tests/QE06.cs
+++ Unity/Assets/Features/Quaternions/Tests/QE06.cs
@@ -7,4 +7,12 @@ using SystemToTest = TestSystem.Quaternion_ex06;
 
 public class QE06 : MonoBehaviour
 {
+    public void INTERPOLATE_BETWEEN_2_QUATERNION()
+    {
+        // Given
+
+        // When
+
+        // Then
+    }
 }
