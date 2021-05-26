96b63f402c8463f14ce4a6860fd7241f3d68e9c3
MadSeche <adrien.ideler@gmail.com>
2021-05-26 14:58:04 +0200

    Second Fix Ex07

--- Unity/Assets/Features/Vector3/Runtime/Vector3_ex07.cs
+++ Unity/Assets/Features/Vector3/Runtime/Vector3_ex07.cs
@@ -9,7 +9,7 @@ namespace Vector3Tests
         /// <summary>
         /// Bon sang, ce foutu miroir me renvoi un reflet distordu.
         /// Je pense qu'en raprochant ma tete a une certaine vitesse, le reflet devrait etre correct.
-        /// Il y a une note sur ce miroir : "interpolant de 1.0f"
+        /// Il y a une note sur ce miroir : "interpolant de 1.0f suivit des initiales PRELS"
         /// </summary>
         public Vector3 Check07(Vector3 baseHeadPosition, Vector3 finalHeadPosition)
         {
