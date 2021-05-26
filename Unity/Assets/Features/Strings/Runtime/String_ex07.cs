6f9237eceeea6d5768008d684d981791ee54f59e
Jean Mourmeaux <58734800+Daxdrena@users.noreply.github.com>
2021-05-26 15:26:43 +0200

    WIP exo7

---
+++ Unity/Assets/Features/Strings/Runtime/String_ex07.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace String_exo07
+{
+    public class String_exo07
+    {
+        #region Main
+        
+
+        #endregion
+    }
+}
