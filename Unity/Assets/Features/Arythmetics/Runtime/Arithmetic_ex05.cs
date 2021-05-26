682b0a05a2ccb8f153994acdf23391f789c6ec6f
EladarArunsun <herve.chiera@gmail.com>
2021-05-26 17:37:24 +0200

    Arithmetics Fix Exercise 05

--- Unity/Assets/Features/Arythmetics/Runtime/Arithmetic_ex05.cs
+++ Unity/Assets/Features/Arythmetics/Runtime/Arithmetic_ex05.cs
@@ -18,13 +18,12 @@ namespace Arithmetics
 
 
         // Ce n'est point fini les amis, à présent, Cherif veut absolument implémenter un moyen de faire grossir
-        // ou diminuer la largeur des vagues selon un temps écoulé entre 0 et 1, avec la valeur de grandeur de début et de fin.
-        // Saachant que la grandeur minimale est de 1m et 50cm.
+        // ou diminuer la largeur des vagues selon un temps écoulé, avec la valeur de grandeur de début et de fin.
+        // Sachant que la grandeur minimale est de 1m et 50cm.
 
         public float CalculateWaves(float value)
         {
-            var variation = Mathf.Lerp(1.5f, m_waveVariationScale, Time.time);
-            return variation;
+            return 0;
         }
     }
 }
