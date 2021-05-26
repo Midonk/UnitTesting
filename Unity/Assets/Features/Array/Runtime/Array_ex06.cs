93f7342bc331b07891f295671458eb7df5e9f3d1
Breniak <kyllian_bocklandt@hotmail.com>
2021-05-26 17:30:31 +0200

    update Array_ex06.cs

--- Unity/Assets/Features/Array/Runtime/Array_ex06.cs
+++ Unity/Assets/Features/Array/Runtime/Array_ex06.cs
@@ -12,16 +12,7 @@ namespace Arrays
 		/// (Oui j'avais pas d'idée)
 		public int[] RevertCode(int[] array)
 		{
-			int[] revertArray = new int[array.Length];
-			int index = 0;
-
-			for (int i = array.Length - 1; i >= 0; i--)
-            {
-				revertArray[index] = array[i];
-				index++;
-			}
-
-			return revertArray;
+			return new int[0];
 		}
 		
 		#endregion
