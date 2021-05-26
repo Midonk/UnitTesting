b05fd2d39a8216df4c7c3e6990d8d22f160856b3
MadSeche <adrien.ideler@gmail.com>
2021-05-26 08:36:23 +0200

    Added Vector3 Ex04

---
+++ Unity/Assets/Features/Vector3/Runtime/Vector3_ex04.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vector3Tests
+{
+    public class Vector3_ex04
+    {
+        #region Main
+
+        /// <summary>
+        /// Calculer la velocite 
+        /// </summary>
+    
+        public Vector3 Check04(Vector3 direction, float speed)
+        {
+            return new Vector3(3, 2, 1);
+        }
+
+        #endregion
+    }    
+}
+
--- Unity/Assets/Features/Vector3/Tests/VECTOR3SHOULD.cs
+++ Unity/Assets/Features/Vector3/Tests/VECTOR3SHOULD.cs
@@ -6,6 +6,7 @@ using UnityEngine.TestTools;
 using TestVector3Ex01 = Vector3Tests.Vector3_ex01;
 using TestVector3Ex02 = Vector3Tests.Vector3_ex02;
 using TestVector3Ex03 = Vector3Tests.Vector3_ex03;
+using TestVector3Ex04 = Vector3Tests.Vector3_ex04;
 
 public class VECTOR3SHOULD
 {
@@ -14,40 +15,56 @@ public class VECTOR3SHOULD
     {
         // Given
         var testVector = new TestVector3Ex01();
-        
+
         // When
-        var result = testVector.Check01(new Vector3(0,0,0));
-        
+        var result = testVector.Check01(new Vector3(0, 0, 0));
+
         // Then
         Assert.IsTrue(result == new Vector3(5, 0, 5));
     }
-    
+
     [Test]
     public void Vector3IsNormal()
     {
         // Given
         var testVector = new TestVector3Ex02();
-        
+
         // When
-        var result = testVector.Check02(new Vector3(5,5,5));
-        
+        var result = testVector.Check02(new Vector3(5, 5, 5));
+
         // Then
         Assert.IsTrue(result == result.normalized);
     }
-    
+
     [Test]
     public void Vector3Distance()
     {
         // Given
         var testVector = new TestVector3Ex03();
-        
+
         // When
         var vect1 = new Vector3(2, 2, 2);
         var vect2 = new Vector3(5, 5, 5);
-        
-        var result = testVector.Check03(vect1,vect2);
-        
+
+        var result = testVector.Check03(vect1, vect2);
+
         // Then
-        Assert.IsTrue(result == Vector3.Distance(vect1,vect2));
+        Assert.IsTrue(result == Vector3.Distance(vect1, vect2));
+    }
+
+    [Test]
+    public void Vector3Velocity()
+    {
+        // Given
+        var testVector = new TestVector3Ex04();
+
+        // When
+        var direction = new Vector3(3, 2, 1);
+        var speed = 2.5f;
+
+        var result = testVector.Check04(direction, speed);
+
+        // Then
+        Assert.IsTrue(result == direction * speed);
     }
 }
