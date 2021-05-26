aa29067bea3ee87a85e47a7c1d614affdba7ae83
maxime.bellens <maxime.bellens@gmail.com>
2021-05-26 10:27:04 +0200

    ex8

--- Unity/Assets/Features/Lists/Runtime/List_ex01.cs
+++ Unity/Assets/Features/Lists/Runtime/List_ex01.cs
@@ -165,7 +165,7 @@ namespace ListSystem
 
 
         /// <summary>
-        /// Rhaaaa. Le stagiaire ne fait que des bétises et au lieu de mettre a jour la liste de produits, il en a créé deux autres avec plein de doublons.
+        /// Rhaaaa. Le stagiaire ne fait que des bétises et au lieu de mettre a jour la liste de produits, il en a créé deux autres ce qui crée plein de doublons.
         /// Aide moi a fusionner tout cà afin qu'il n'y ait plus qu'une liste mais sans les doublons
         /// </summary>
         /// 
