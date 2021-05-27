8888f0454068a072245171647f1379d6ee1524b1
EladarArunsun <herve.chiera@gmail.com>
2021-05-27 09:31:40 +0200

    Arithmetics Exercise 08

---
+++ Unity/Assets/Features/Arythmetics/Runtime/Arithmetic_ex08.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arithmetics
+{
+    public class Arithmetic_ex08
+    {
+
+        // Cherif arrive au bout de sa course et se rend compte qu'il a fait tout ce chemin en sens inverse...
+        // Ravi, il doit recalculer son trajet dans le bon sens sachant qu'il devra se taper deux fois le trajet
+        // en s'assurant que le calcul soit dans une valeur positive.
+
+
+        public float CalculatePositiveTime(float beachLength, float walkSpeed)
+        {
+            return 0;
+        }
+    }
+}
