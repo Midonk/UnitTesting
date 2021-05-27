736419346ceece8b6e399bafd5b8fb4bbc07381a
Amaury Deltenre <amaury.deltenre@gmail.com>
2021-05-27 10:07:50 +0200

    Rebase test file in one

--- Unity/Assets/Features/Floats/Tests/FloatTest.cs
+++ Unity/Assets/Features/Floats/Tests/FloatTest.cs
@@ -4,8 +4,9 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using TestFloat01 = Float.Float_ex01;
+using TestFloat02 = Float.Float_ex02;
 
-public class FloatTest01
+public class FloatTest
 {
     [Test]
     public void TEST_FLOAT_EX01()
@@ -19,5 +20,18 @@ public class FloatTest01
 
         // Then
         Assert.IsTrue(Mathf.Approximately(expected, years));
+    }
+
+    [Test]
+    public void TEST_FLOAT_EX02()
+    {
+        // Given
+
+
+        // When
+
+
+        // Then
+
     }
 }
